3868dc6e5962673a61e566fe136e0fa21e27475a
Anton Krupin <krupin_anton@mail.ru>
2022-12-12 20:31:22 +0300

    add import PremiersList from './PremiersList';

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+
+import PremiersList from './PremiersList';
+
 import './App.css';
 
 const App = () => {
   return (
-    <h1>Hello</h1>
+    <><PremiersList /></>
   )
 }
 
