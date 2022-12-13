250ea0cac6291f0992ca7c166a1a3b8c01bce2ab
antonkrupin <krupin_anton@mail.ru>
2022-12-13 10:41:31 +0300

    add translations

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,6 +2,33 @@ export default {
   translation: {
     ui: {
       title: 'Список премьер фильмов',
+      loading: 'Загрузка списка фильмов...',
+      buttonLoad: 'Загрузить',
+    },
+    filmItem: {
+      year: 'Год выхода:',
+      duration: 'Продолжительность:',
+      minutes: ' мин',
+    },
+    mounthSelector: {
+      default: 'Выберите месяц',
+      mounth: {
+        january: 'Январь',
+        february: 'Февраль',
+        march: 'Март',
+        april: 'Апрель',
+        may: 'Май',
+        june: 'Июнь',
+        july: 'Июль',
+        august: 'Август',
+        september: 'Сентябрь',
+        october: 'Октябрь',
+        november: 'Ноябрь',
+        december: 'Декабрь',
+      },
+    },
+    yearSelector: {
+      default: 'Выберите год',
     },
     errors: {
 
