704efafa948bbd0a5743ecac6282ec33503a945f
Anton Krupin <krupin_anton@mail.ru>
2022-12-13 19:53:26 +0300

    create InfoBlock

---
+++ src/components/InfoBlock.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import i18n from '../asserts/i18next';
+import { selectFetchStatus } from '../store/selectors';
+
+const InfoBlock = () => {
+  const status = useSelector(selectFetchStatus);
+
+  switch (status) {
+    case 'loading': {
+      return (
+        <div className="d-flex align-items-center justify-content-center">
+          <span className="spinner-border text-primary mr-5" role="status" aria-hidden="true" />
+          <span className="text-primary">{i18n.t('ui.loading')}</span>
+        </div>
+      );
+    }
+    case null: {
+      return (
+        <div className="d-flex justify-content-center">{i18n.t('ui.noFilmsTitle')}</div>
+      );
+    }
+    default: {
+      return (
+        <div />
+      );
+    }
+  }
+};
+
+export default InfoBlock;
