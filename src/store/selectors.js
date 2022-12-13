9c267f7bcc59b7a888d6bd45fce335c8be7562dc
antonkrupin <krupin_anton@mail.ru>
2022-12-13 15:30:39 +0300

    add selectors

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,6 +2,8 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const selectAllFilms = (state) => state.films.films;
 export const selectActiveFilter = (state) => state.films.filter;
+export const selectFetchStatus = (state) => state.films.fetchStatus;
+export const selectQueryParams = (state) => state.films.params;
 
 export const selectFilmsByFilter = createSelector(
   [selectAllFilms, selectActiveFilter],
