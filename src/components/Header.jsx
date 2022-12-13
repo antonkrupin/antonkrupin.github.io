7a0ea466dbfadb68ea67bef7a0729267ed4a3b31
antonkrupin <krupin_anton@mail.ru>
2022-12-13 10:42:25 +0300

    add selectors components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,41 +3,57 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import i18n from '../asserts/i18next';
 
-import { fetchFilms } from '../store/filmsSlice';
+import MounthSelector from './selectors/MounthSelector';
+import YearSelector from './selectors/YearSelector';
+
+import routes, { headers } from '../routes';
+import { fetchFilms, clearFilmsList } from '../store/filmsSlice';
 
 const Header = () => {
   const dispatch = useDispatch();
 
   const status = useSelector((state) => state.films.fetchStatus);
 
+  const params = useSelector((state) => state.films.params);
+
+  const fetchFilmsHandler = () => {
+    const { year, month } = params;
+    if (!year || !month) {
+      console.log('bad');
+    } else {
+      dispatch(clearFilmsList());
+      const requestOptions = [routes.premiersPath(),
+        {
+          headers,
+          params,
+        }];
+      dispatch(fetchFilms(requestOptions));
+    }
+  };
+
   return (
     <>
       <div className="d-flex text-center justify-content-center flex-column">
         <h2 className="text-primary">{i18n.t('ui.title')}</h2>
-        { status === 'loading'
+      </div>
+      <div className="d-flex w-50 m-auto mt-5">
+        <MounthSelector />
+        <YearSelector />
+        <button
+          onClick={fetchFilmsHandler}
+          className="btn btn-outline-primary m-2"
+          type="button"
+        >
+          {i18n.t('ui.buttonLoad')}
+        </button>
+      </div>
+      { status === 'loading'
       && (
       <div className="d-flex align-items-center justify-content-center">
         <span className="spinner-border text-primary mr-5" role="status" aria-hidden="true" />
-        <span className="text-primary">Загрузка списка...</span>
+        <span className="text-primary">{i18n.t('ui.loading')}</span>
       </div>
       ) }
-      </div>
-      <button className="btn btn-outline-primary" type="button" onClick={() => dispatch(fetchFilms())}>Загрузить</button>
-      <select className="form-select" aria-label="Default select example">
-        <option>Выберите месяц</option>
-        <option value="JANUARY">Январь</option>
-        <option value="FEBRUARY">Февраль</option>
-        <option value="MARCH">Март</option>
-        <option value="APRIL">Апрель</option>
-        <option value="MAY">Май</option>
-        <option value="JUNE">Июнь</option>
-        <option value="JULY">Июль</option>
-        <option value="AUGUST">Август</option>
-        <option value="SEPTEMBER">Сентябрь</option>
-        <option value="OCTOBER">Октябрь</option>
-        <option value="NOVEMBER">Ноябрь</option>
-        <option value="DECEMBER">Декабрь</option>
-      </select>
     </>
   );
 };
