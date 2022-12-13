2b3e694c6603ae7d9622015d960c718d6137f4b7
antonkrupin <krupin_anton@mail.ru>
2022-12-13 12:57:24 +0300

    remove header component

--- src/components/PremiersList.jsx
+++ src/components/PremiersList.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import Header from './Header';
 import FilmItem from './FilmItem';
 
 const PremiersList = () => {
   const films = useSelector((state) => state.films.films);
 
   return (
-    <>
-      <Header />
-      <div className="d-flex flex-wrap justify-content-center">
-        {films.map((film) => (
-          <FilmItem
-            key={film.kinopoiskId}
-            film={film}
-          />
-        ))}
-      </div>
-    </>
+    <div className="d-flex flex-wrap justify-content-center">
+      {films.map((film) => (
+        <FilmItem
+          key={film.kinopoiskId}
+          film={film}
+        />
+      ))}
+    </div>
   );
 };
 
