a896a2043e37c790826b7d23c92748c6a8c56814
antonkrupin <krupin_anton@mail.ru>
2022-12-15 09:37:04 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Header from './components/Header';
 import PremiersList from './components/PremiersList';
 
-import './App.css';
+import { selectFetchStatus, selectQueryParams, selectActiveFilter } from './store/selectors';
 
-const App = () => (
-  <>
-    <Header />
-    <PremiersList />
-  </>
-);
+const App = () => {
+  const status = useSelector(selectFetchStatus);
+
+  const params = useSelector(selectQueryParams);
+
+  const filter = useSelector(selectActiveFilter);
+  return (
+    <>
+      <Header status={status} params={params} filter={filter} />
+      <PremiersList />
+    </>
+  );
+};
 
 export default App;
