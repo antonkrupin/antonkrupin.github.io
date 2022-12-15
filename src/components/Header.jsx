39d194180235430babf18304e3c72859cbd4b802
antonkrupin <krupin_anton@mail.ru>
2022-12-15 10:28:58 +0300

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
 
 import i18n from '../asserts/i18next';
 import Button from './Button';
 import InfoBlock from './InfoBlock';
-import { YearSelector, MonthSelector } from './SelectComponent';
-// import SelectComponent from './SelectComponent';
-// import { selectFetchStatus, selectQueryParams, selectActiveFilter } from '../store/selectors';
+import SelectComponent from './SelectComponent';
 import '../style/Header.css';
 
 const Header = (props) => {
   const { status, params, filter } = props;
-  /* const status = useSelector(selectFetchStatus);
 
-  const params = useSelector(selectQueryParams);
-
-  const filter = useSelector(selectActiveFilter); */
   return (
     <>
       <div className="d-flex bg-warning p-2 text-center justify-content-center flex-column">
@@ -23,8 +16,8 @@ const Header = (props) => {
       </div>
       <div className="filters-container m-auto mt-5">
         <div className="filters">
-          <MonthSelector status={status} />
-          <YearSelector status={status} />
+          <SelectComponent type="month" status={status} />
+          <SelectComponent type="year" status={status} />
           <Button
             type="load"
             filter={filter}
--- src/components/SelectComponent.jsx
+++ src/components/SelectComponent.jsx
@@ -7,77 +7,7 @@ import { setFetchStatus, setSearchParams, setTopCoord } from '../store/filmsSlic
 
 import locale from '../locales/ru';
 
-export const YearSelector = (props) => {
-  const dispatch = useDispatch();
-
-  const { status } = props;
-
-  const disabled = status === 'loading';
-
-  const years = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011];
-
-  const selectHandler = (e) => {
-    dispatch(setFetchStatus('changeFilters'));
-    dispatch(setSearchParams({ paramName: 'year', value: e.target.value }));
-    dispatch(setTopCoord(window.pageYOffset));
-  };
-
-  return (
-    <select
-      disabled={disabled}
-      onChange={(e) => selectHandler(e)}
-      className="form-select m-2"
-      aria-label={i18n.t('yearSelector.ariaLabel')}
-    >
-      <option>{i18n.t('yearSelector.default')}</option>
-      {years.map((year) => (
-        <option
-          key={year}
-          value={year.toString()}
-        >
-          {year}
-        </option>
-      ))}
-    </select>
-  );
-};
-
-export const MonthSelector = (props) => {
-  const dispatch = useDispatch();
-
-  const { status } = props;
-
-  const disabled = status === 'loading';
-
-  const { month } = locale.translation.monthSelector;
-
-  const selectHandler = (e) => {
-    dispatch(setFetchStatus('changeFilters'));
-    dispatch(setSearchParams({ paramName: 'month', value: e.target.value }));
-    dispatch(setTopCoord(window.pageYOffset));
-  };
-
-  return (
-    <select
-      disabled={disabled}
-      onChange={(e) => selectHandler(e)}
-      className="form-select m-2"
-      aria-label={i18n.t('monthSelector.ariaLabel')}
-    >
-      <option>{i18n.t('monthSelector.default')}</option>
-      {Object.keys(month).map((key) => (
-        <option
-          key={key}
-          value={key.toUpperCase()}
-        >
-          {month[key]}
-        </option>
-      ))}
-    </select>
-  );
-};
-
-/* const SelectComponent = (props) => {
+const SelectComponent = (props) => {
   const dispatch = useDispatch();
 
   const { type, status } = props;
@@ -91,6 +21,7 @@ export const MonthSelector = (props) => {
   const selectHandler = (e) => {
     dispatch(setFetchStatus('changeFilters'));
     dispatch(setSearchParams({ paramName: type, value: e.target.value }));
+    dispatch(setTopCoord(window.pageYOffset));
   };
 
   switch (type) {
@@ -140,4 +71,4 @@ export const MonthSelector = (props) => {
   }
 };
 
-export default SelectComponent; */
+export default SelectComponent;
