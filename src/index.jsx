d3b2487b8858ea102aed3c308d9045488b2c0111
antonkrupin <krupin_anton@mail.ru>
2022-12-15 09:36:38 +0300

    add import runApp from './init'

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import runApp from './init';
 
-import App from './App';
-import store from './store/index';
-
-import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
+runApp();
