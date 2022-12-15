d3ddfb8039fadae3cadbafb05e554976fd13c75a
antonkrupin <krupin_anton@mail.ru>
2022-12-15 10:09:41 +0300

    add option generation for select

--- src/components/SelectComponent.jsx
+++ src/components/SelectComponent.jsx
@@ -5,12 +5,18 @@ import i18n from '../asserts/i18next';
 
 import { setFetchStatus, setSearchParams } from '../store/filmsSlice';
 
+import locale from '../locales/ru';
+
 const SelectComponent = (props) => {
   const dispatch = useDispatch();
 
   const { type, status } = props;
 
   const disabled = status === 'loading';
+
+  const years = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011];
+
+  const { month } = locale.translation.monthSelector;
 
   const selectHandler = (e) => {
     dispatch(setFetchStatus('changeFilters'));
@@ -27,18 +33,14 @@ const SelectComponent = (props) => {
           aria-label={i18n.t('yearSelector.ariaLabel')}
         >
           <option>{i18n.t('yearSelector.default')}</option>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-          <option value="2018">2018</option>
-          <option value="2017">2017</option>
-          <option value="2016">2016</option>
-          <option value="2015">2015</option>
-          <option value="2014">2014</option>
-          <option value="2013">2013</option>
-          <option value="2012">2012</option>
-          <option value="2011">2011</option>
+          {years.map((year) => (
+            <option
+              key={year}
+              value={year.toString()}
+            >
+              {year}
+            </option>
+          ))}
         </select>
       );
     }
@@ -51,18 +53,14 @@ const SelectComponent = (props) => {
           aria-label={i18n.t('monthSelector.ariaLabel')}
         >
           <option>{i18n.t('monthSelector.default')}</option>
-          <option value="JANUARY">{i18n.t('monthSelector.month.january')}</option>
-          <option value="FEBRUARY">{i18n.t('monthSelector.month.february')}</option>
-          <option value="MARCH">{i18n.t('monthSelector.month.march')}</option>
-          <option value="APRIL">{i18n.t('monthSelector.month.april')}</option>
-          <option value="MAY">{i18n.t('monthSelector.month.may')}</option>
-          <option value="JUNE">{i18n.t('monthSelector.month.june')}</option>
-          <option value="JULY">{i18n.t('monthSelector.month.july')}</option>
-          <option value="AUGUST">{i18n.t('monthSelector.month.august')}</option>
-          <option value="SEPTEMBER">{i18n.t('monthSelector.month.september')}</option>
-          <option value="OCTOBER">{i18n.t('monthSelector.month.october')}</option>
-          <option value="NOVEMBER">{i18n.t('monthSelector.month.november')}</option>
-          <option value="DECEMBER">{i18n.t('monthSelector.month.december')}</option>
+          {Object.keys(month).map((key) => (
+            <option
+              key={key}
+              value={key.toUpperCase()}
+            >
+              {month[key]}
+            </option>
+          ))}
         </select>
       );
     }
